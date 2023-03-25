eac57f7dd1c6addbfd839566e93222bf21773eba
Bohdan <borsh810@gmail.com>
2023-03-25 16:27:04 +0200

    complete

--- src/components/Manual/Manual.tsx
+++ src/components/Manual/Manual.tsx
@@ -1,25 +1,42 @@
 import SurnameX from "./SurnameX"
 import Ukrainian from "../Characters/Ukrainian"
-import Russian from "../Characters/Russian"
-import Belarus from "../Characters/Belorus"
-import Kazakh from '../Characters/Kazakh'
+
+const data = {
+	uk: {
+		 nation: 'Україна',
+		 ends: ['ун', 'ка', 'ач', 'ок', 'юх', 'ах', 'ыш', 'ух', 'ня', 'ина', 'ьо', 'ля', 'та', 'ха', 'ца', 'ака', 'яка', 'га', 'ань', 'аш', 'шин', 'хно', 'ан', 'ян', 'ло', 'ко', 'ра', 'ла', 'ба', 'да', 'ец', 'ей', 'ай', 'ій', 'ар', 'арь', 'ейк', 'очк', 'ар', 'як', 'коваль', 'билоус']
+	},
+	ru: {
+		 nation: 'Росія',
+		 ends: ['ый', 'ин', 'ын', 'ев', 'ёв', 'ов', 'ой', 'их', 'ых']
+	},
+	by: {
+		 nation: 'Білорусь',
+		 ends: ['ик', 'ык', 'онок', 'енок', 'чик']
+	},
+	kz: {
+		 nation: 'Казахстан',
+		 ends: ['ұлы', 'қызы', 'улы', 'кызы', 'тегі']
+	}
+}
 
 const Manual = (props: any) => {
-	const basic = props.surname
-	const surname = props.surname.toLowerCase().replace(/[^а-яіїґёʼ'ўұқ-]/ig, '')
-	const two = surname.slice(-2)
-	const sur = surname.slice(-3)
-	const ukrDop = surname.slice(-4)
-	if (basic.length > 29) return <div><span>Упс... Занадто довге прізвище</span></div>
-	if (surname.length > 4) {
-		if (two === 'ун' || two === 'ка' || two === 'ач' || two === 'ок' || two === 'юх' || two === 'ах' || two === 'ыш' || two === 'ух' || two === 'ня' || sur === 'ина' || two === 'ьо' || two === 'ля' || two === 'та' || two === 'ха' || two === 'ца' || sur === 'ака' || sur === 'яка' || two === 'га' || sur === 'ань' || two === 'аш' || sur === 'шин' || sur === 'хно' || two === 'ан' || two === 'ян' || two === 'ло' || two === 'ко' || two === 'ра' || two === 'ла' || two === 'ба' || two === 'да' || two === 'ец' ||  two === 'ей' || two === 'ай' || two === 'ій' || two === 'ар' || sur === 'арь' || ukrDop.includes('ейк') || ukrDop.includes('очк') || two === 'ар' || two === 'як' ||  surname === 'коваль' ||  surname === 'билоус' ) return <div><SurnameX story={<Ukrainian></Ukrainian>} nation='Україна' surname={props.surname}></SurnameX></div>
-		if (two === 'ый' || two === 'ин' || two === 'ын' || two === 'ев' || two === 'ёв' || two === 'ов' || two === 'ой' || two === 'их' || two === 'ых') return <div><SurnameX story={<Russian></Russian>} nation='Росія' surname={props.surname}></SurnameX></div>
-		if (two === 'ик' || two === 'ык' || ukrDop === 'онок' || ukrDop === 'енок' || sur === 'чик') return <div><SurnameX story={<Belarus></Belarus>} nation='Білорусь' surname={props.surname}></SurnameX></div>
-		if (sur === 'ұлы' || ukrDop === 'қызы' || sur === 'улы' || ukrDop === 'кызы' || ukrDop === 'тегі' ) return <div><SurnameX story={<Kazakh></Kazakh>} nation='Казахстан' surname={props.surname}></SurnameX></div>
-	}
-	return(
-		<></>
-	)
+	
+    const surname = props.surname // Витягаємо прізвище з props
+	
+    if (surname.length > 29) return <span>Упс... Занадто довге прізвище</span> // Перевіряємо чи не перевищує довжину прізвища 29 символів
+    else if (surname.length < 4) return <span>Упс... Занадто коротке прізвище</span> // Перевіряємо чи не менше 4 символів
+
+
+    const country = Object.values(data).find((country) => { // Для кожного об'єкта в об'єкті data
+        const {ends} = country // Витягаємо масив ends з об'єкта country
+        return ends.find((end) => surname.endsWith(end)) // Перевіряємо чи прізвище закінчується на один з елементів масиву ends
+    })
+
+    if (!country) return <span>Упс... Прізвище не визначене</span> // Якщо не належить до жодної з країн, то повертаємо повідомлення
+
+    const {nation} = country // Витягаємо з об'єкта country значення nation
+    return <SurnameX story={<Ukrainian/>} nation={nation} surname={surname}></SurnameX> // Повертаємо компонент SurnameX з пропсами
 }
 
 export default Manual
